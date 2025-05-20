67e27ba12fd04bc2a9ac578041888bec88caa7fb
codephilosopher <codephilosopher000@gmail.com>
2025-05-20 18:50:01 +0530

    added simple web app in go

--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+const PORT = ":8080"
 
 func main() {
-	fmt.Println("Hello World!")
+	// fmt.Println("Hello World")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintln(w, "Hello World!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("Number of bytes written %d\n", n)
+	})
+	fmt.Printf("Sever listening to the port %s\n", PORT)
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
