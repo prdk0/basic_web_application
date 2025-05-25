8bf4b5f3e39cd9ada709164506894d5d736eebd8
codephilosopher <codephilosopher000@gmail.com>
2025-05-25 12:40:51 +0530

    added examples of session management with SCS session management

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -23,12 +23,16 @@ func NewHandlers(r *Repository) {
 	Repo = r
 }
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remoteip", remoteIP)
 	render.RenderTemplates(w, "home.page.html", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := map[string]string{}
-	stringMap["test"] = "Hello to About Page!"
+	// stringMap["test"] = "Hello to About Page!"
+	remoteIP := m.App.Session.GetString(r.Context(), "remoteip")
+	stringMap["remote_ip"] = remoteIP
 	render.RenderTemplates(w, "about.page.html", &models.TemplateData{
 		StringMap: stringMap,
 	})
