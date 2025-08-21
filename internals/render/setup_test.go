53a39870682600e27e5ba46d8bc268d430c9a145
codephilosopher <codephilosopher000@gmail.com>
2025-08-21 19:49:12 +0530

    added test for render packges

---
+++ internals/render/setup_test.go
@@ -0,0 +1,34 @@
+package render
+
+import (
+	"bookings/internals/config"
+	"bookings/internals/models"
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	gob.Register(models.Reservation{})
+
+	// setting app enviroment
+	testApp.InProduction = false
+
+	// Session settings
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+	testApp.Session = session
+
+	app = &testApp
+	os.Exit(m.Run())
+}
