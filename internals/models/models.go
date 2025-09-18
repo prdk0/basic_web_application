361826b04b30af4ff38d207240f7b4a721dbf999
codephilosopher <codephilosopher000@gmail.com>
2025-09-18 17:40:09 +0530

    added all models 04_13

--- internals/models/models.go
+++ internals/models/models.go
@@ -1,8 +1,52 @@
 package models
 
+import "time"
+
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Room
+}
+
+type User struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Room struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Restriction struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type RoomRestriction struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	Room          Room
+	Reservation   Reservation
+	Restriction   Restriction
 }
