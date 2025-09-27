8a8aafb78e8b4f07b5618d1999b8b4b6967bc3bd
pradeek <pradeek.k@gmail.com>
2025-09-27 19:05:04 +0530

    completed 20_14

---
+++ migrations/20250927132330_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions(
+	restriction_name, created_at, updated_at)
+	VALUES ('Reservation', '2025-09-13 00:00:00', '2025-09-13 00:00:00'), ('Owner Block', '2025-09-21 00:00:00', '2025-09-21 00:00:00');
