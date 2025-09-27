c88e470e6e76e6d889163733066e8c88cc452563
pradeek <pradeek.k@gmail.com>
2025-09-27 18:53:13 +0530

    completed 20_13

---
+++ migrations/20250927130240_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.rooms(
+	room_name, created_at, updated_at)
+	VALUES 
+    ('Generals Quarters', '2025-09-21 00:00:00', '2025-09-21 00:00:00'), 
+    ('Majors Suite', '2025-09-21 00:00:00', '2025-09-21 00:00:00');
