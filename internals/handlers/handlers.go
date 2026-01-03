13937779a160a62a95cb8fc50d4a7e7447096aae
pradeek <pradeek.k@gmail.com>
2026-01-03 12:38:16 +0530

    15_17 reservation calendar 4

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -734,7 +734,7 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 				}
 			} else {
 				// block
-				blockMap[y.StartDate.Format("2006-01-2")] = y.RestrictionID
+				blockMap[y.StartDate.Format("2006-01-2")] = y.ID
 			}
 		}
 
